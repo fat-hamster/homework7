8d1cfc3272a1fd12d4be22afe16fa6d2d54f1300
Dmitriy Gridchin <tigris.tstu@gmail.com>
2020-12-15 21:33:38 +0300

    чуть добавил проверок

--- src/Plate.java
+++ src/Plate.java
@@ -1,12 +1,20 @@
+import java.util.Random;
+
 public class Plate {
     private int food;
+    private final int MAX_FOOD;
 
     public Plate(int food) {
         this.food = food;
+        MAX_FOOD = new Random().nextInt(30) + 40;
     }
 
     public void addFood(int food) {
         this.food += food;
+        if(this.food > MAX_FOOD) {
+            System.out.println("Тарелка полна!");
+            this.food = MAX_FOOD;
+        }
     }
 
     public int decFood(int food) {
